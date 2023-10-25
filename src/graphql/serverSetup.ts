9fa20268560e97a981e4dd1217206ad7a6c92c05
Michael Nusair <mnpcmw6444@gmail.com>
2023-10-25 16:09:55 +0300

    init

--- src/graphql/serverSetup.ts
+++ src/graphql/serverSetup.ts
@@ -4,7 +4,8 @@ import schema from "./schema";
 import jsonwebtoken from "jsonwebtoken";
 import settings from "../settings";
 import userModel from "../mongo/auth/userModel";
-
+import cors from 'cors';
+import cookieParser  from "cookie-parser"
 export default async () => {
 
 
@@ -15,53 +16,36 @@ export default async () => {
 
             let token;
 
-            // Attempt to extract token from Authorization header first
-            const authHeader = req.headers.authorization;
 
-            if (authHeader) {
-
-                token = authHeader.split('Bearer ')[1];
-                if (!token) {
-                    token = null;
-
-                }
-            }
-            // If not in header, then attempt to extract from cookie
-            else if (req.cookies && req.cookies.jwt) {
-
+            if (req.cookies && req.cookies.jwt) {
                 token = req.cookies.jwt;
             } else {
-
                 token = null;
-
             }
-
-
             let decoded;
             try {
                 decoded = token ? jsonwebtoken.verify(token, settings.jwtSecret) : null;
             } catch (err) {
                 decoded = null;
             }
-
             if (typeof decoded !== 'object' || !decoded?.id) {
                 return {req, res, user: null};
             }
-
             const user = await userModel().findById(decoded.id);
             if (!user) {
                 return {req, res, user: null};
             }
-
             return {req, res, user};
-        },
-        introspection: settings.env === "local",  // Ensure introspection is on, especially if in production mode
+        }, introspection: settings.env === "local",
     });
 
     const app = express();
 
+    app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+    app.use(cookieParser());
+
     await server.start()
-    server.applyMiddleware({app, path: '/'});
+    server.applyMiddleware({app, path: '/', cors: false});
 
     app.listen({port: 6005}, () =>
         console.log(`🚀 Server ready at http://localhost:6005${server.graphqlPath}`)
