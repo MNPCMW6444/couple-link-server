60247b72943a327341a52c4bf7991c564484fd0b
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-02 00:37:46 +0200

    Revert "local"
    This reverts commit 101baf9c

--- src/ai/ai.ts
+++ src/ai/ai.ts
@@ -50,7 +50,7 @@ export const fireAI = async (sessionId: string) => {
 
 
         const completion = await openai.chat.completions.create({
-            model: "gpt-4",
+            model: "gpt-3.5-turbo",
             messages: [
                 {
                     role: "system",
