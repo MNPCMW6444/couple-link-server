8ec973f2dfd216424969c00fcf5aa2df1ce7a612
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-09 10:11:22 +0200

    Revert "local"
    This reverts commit 101baf9c

--- src/mongo/connection.ts
+++ src/mongo/connection.ts
@@ -1,19 +1,16 @@
-import mongoose, { ConnectOptions } from "mongoose";
+import mongoose from "mongoose";
 import settings from "../settings";
 
 export let connection: mongoose.Connection | null = null;
 
 export const connect = async () => {
-  try {
-    await mongoose.connect(settings.mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    } as ConnectOptions);
-    console.log("Mongo DB connected successfully");
-    connection = mongoose.connection;
-  } catch (err) {
-    console.error("mongo connection error:" + err.message);
-    throw new Error(err);
-  }
+    try {
+        await mongoose.connect(settings.mongoURI);
+        console.log("Mongo DB connected successfully");
+        connection = mongoose.connection;
+    } catch (err) {
+        console.error("mongo connection error:" + err.message);
+        throw new Error(err);
+    }
 };
 
